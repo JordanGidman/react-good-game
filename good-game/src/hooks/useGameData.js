46270bba0a0daacf3ad1ae35306582d7667916fb
Jordan Gidman <jordanleegidman@hotmail.com>
2024-07-17 14:39:57 +0100

    Boilerplating
    I have added some boiler plate files that i know i will need during the process, i will add others as and when i think of them along the way. For now this is the baseline. I have looked around for a suitable API for the games, however i may have to pivot either to TV Shows instead of games or to a web scraper to get the data.

--- good-game/src/hooks/useGameData.js
+++ good-game/src/hooks/useGameData.js
@@ -1,13 +1,22 @@
 import { useEffect, useState } from "react";
+import { API_KEY } from "../data/API_KEY.js";
 
 export function useGameData() {
   const [steamData, setSteamData] = useState({});
 
-  // useEffect(() => {
-  //   fetch("")
-  //     .then((res) => res.json())
-  //     .then((data) => setSteamData(data));
-  // }, []);
+  useEffect(() => {
+    fetch(`https://www.giantbomb.com/api/games/?api_key=${API_KEY}`, {
+      method: "GET",
+      "Content-Type": "application/json",
+      mode: "no-cors",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.json);
+
+        setSteamData(data);
+      });
+  }, []);
 
   return steamData;
 }
